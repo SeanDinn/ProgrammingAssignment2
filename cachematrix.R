4ffe9077c80ec48438cb11a7901becb4db9b1ca6
Sean <dinn@ge.com>
2015-10-23 12:25:07 -0400

    First revision of file-desscriptors added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will take a matrix and invert it. It will first test to make sure it has not 
+## already been inverted. If it has, it will retreive the matrix from a cache
 
-## Write a short comment describing this function
+## This function will create a matrix, calculate the inverse and cache it
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function will determine if the input matrix has been inverted. If it has, it will 
+## retrieve the inverted matrix from the cache. If not, it will invert the input matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
 }
